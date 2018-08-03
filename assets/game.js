2ade43c3e69c3255b58a7fe6b99bea01756c67e8
charliesands <cwsands3@gmail.com>
2018-08-03 17:09:04 -0600

    83-509

--- assets/game.js
+++ assets/game.js
@@ -1,23 +1,52 @@
-let health = 100;
+let gameStats = {
+  health: 100,
+  targetName: "Mike Tyson",
+  hits: 0,
+  items: []
+}
+
+let items = {
+  removePadding: { name: "Padding Removal", modifier: 2, description: "Remove padding from gloves" },
+  peds: { name: "PED's", modifier: 5, description: "Performance Enhancing Drugs" },
+  cheatCode: { name: "Cheat Code!", modifier: 10, description: "Cheaters always win" }
+}
 
 function headShot() {
-  health = health - 1;
+  gameStats.health = gameStats.health -= 1 + addMods();
+  gameStats.hits++
   update()
 }
 
 function bodyShot() {
-  health = health - 5;
+  gameStats.health = gameStats.health - 5;
+  gameStats.hits++
   update()
 }
 
 function upperCut() {
-  health = health - 10;
+  gameStats.health = gameStats.health - 10;
+  gameStats.hits++
   update()
 }
 
+function givePeds() {
+  gameStats.items.push(items.peds)
+}
+
+function addMods() {
+  let modTotal = 0
+  for (let i = 0; i < gameStats.items.length; i++) {
+    let mod = gameStats.items[i].modifier;
+    modTotal += mod
+  }
+  return modTotal;
+}
+
 function update() {
-  return document.getElementById("health").innerText = health.toString();
+  document.getElementById("hits").innerText = gameStats.hits.toString();
+  document.getElementById("health").innerText = gameStats.health.toString();
+  document.getElementById("target-name").innerText = gameStats.targetName;
 }
 
 update();
-headShot();
+
