8c00bf53c98cc03571765538f771726e3082365b
charliesands <cwsands3@gmail.com>
2018-08-04 17:27:50 -0600

    84-527

--- assets/game.js
+++ assets/game.js
@@ -1,36 +1,85 @@
 let gameStats = {
-  health: 100,
+  health: 1000,
   targetName: "Mike Tyson",
   hits: 0,
   items: []
 }
 
 let items = {
-  removePadding: { name: "Padding Removal", modifier: 2, description: "Remove padding from gloves" },
-  peds: { name: "PED's", modifier: 5, description: "Performance Enhancing Drugs" },
-  cheatCode: { name: "Cheat Code!", modifier: 10, description: "Cheaters always win" }
+  removePadding: { name: "Padding Removal", modifier: 10, description: "Remove padding from gloves" },
+  peds: { name: "PED's", modifier: 25, description: "Performance Enhancing Drugs" },
+  cheatCode: { name: "Cheat Code!", modifier: 250, description: "Cheaters always win" }
+}
+function reset() {
+  gameStats.health = 1000
+  gameStats.hits = 0
+  gameStats.items = []
+  clickedPeds = 0;
+  clicked = 0;
 }
 
+
+
 function headShot() {
-  gameStats.health = gameStats.health -= 1 + addMods();
+  gameStats.health = gameStats.health -= 10 + addMods();
   gameStats.hits++
   update()
 }
 
 function bodyShot() {
-  gameStats.health = gameStats.health - 5;
+  gameStats.health = gameStats.health -= 15 + addMods();
   gameStats.hits++
   update()
 }
 
 function upperCut() {
-  gameStats.health = gameStats.health - 10;
+  gameStats.health = gameStats.health -= 30 + addMods();
   gameStats.hits++
   update()
 }
 
+let clickedPeds = 0
 function givePeds() {
-  gameStats.items.push(items.peds)
+  if (clickedPeds++ < 5) {
+    gameStats.items.push(items.peds);
+  }
+  else {
+    alert("Dude! You're dead!")
+    reset()
+    update()
+  }
+
+
+  update()
+}
+
+let clickPad = 0;
+function giveRemovePadding() {
+  if (clickPad++ < 20) {
+    gameStats.items.push(items.removePadding)
+  }
+  else {
+    alert("Good job! Your hand is broken.")
+    reset()
+    update()
+  }
+  update()
+}
+
+let clicked = 0
+function giveCheatCode() {
+
+  if (clicked++ < 3) {
+    gameStats.items.push(items.cheatCode);
+  }
+  else {
+    alert("Really? C'mon man. You lose!")
+    reset()
+    update()
+  }
+
+
+  update()
 }
 
 function addMods() {
